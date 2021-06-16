3d575d6a2aa2f28cb930272fb436d50c54f7e9a3
Runjia Tian <runjiatian@gmail.com>
2021-06-16 15:00:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CityGAN
+ Accessible Toolkit for Urban GIS Data Scraping, Wrangling and Real-Time Generative Inference
---
+++ gan_server.py
@@ -0,0 +1,129 @@
+from flask import Flask, request, jsonify
+from json import loads, dumps
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from scipy import ndimage
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+OUTPUT_CHANNELS = 3
+def downsample(filters, size, apply_batchnorm=True):
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  result = tf.keras.Sequential()
+  result.add(
+      tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
+                             kernel_initializer=initializer, use_bias=False))
+
+  if apply_batchnorm:
+    result.add(tf.keras.layers.BatchNormalization())
+
+  result.add(tf.keras.layers.LeakyReLU())
+
+  return result
+
+def upsample(filters, size, apply_dropout=False):
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  result = tf.keras.Sequential()
+  result.add(
+    tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
+                                    padding='same',
+                                    kernel_initializer=initializer,
+                                    use_bias=False))
+
+  result.add(tf.keras.layers.BatchNormalization())
+
+  if apply_dropout:
+      result.add(tf.keras.layers.Dropout(0.5))
+
+  result.add(tf.keras.layers.ReLU())
+
+  return result
+
+def Generator():
+  inputs = tf.keras.layers.Input(shape=[256,256,3])
+
+  down_stack = [
+    downsample(64, 4, apply_batchnorm=False), # (bs, 128, 128, 64)
+    downsample(128, 4), # (bs, 64, 64, 128)
+    downsample(256, 4), # (bs, 32, 32, 256)
+    downsample(512, 4), # (bs, 16, 16, 512)
+    downsample(512, 4), # (bs, 8, 8, 512)
+    downsample(512, 4), # (bs, 4, 4, 512)
+    downsample(512, 4), # (bs, 2, 2, 512)
+    downsample(512, 4), # (bs, 1, 1, 512)
+  ]
+
+  up_stack = [
+    upsample(512, 4, apply_dropout=True), # (bs, 2, 2, 1024)
+    upsample(512, 4, apply_dropout=True), # (bs, 4, 4, 1024)
+    upsample(512, 4, apply_dropout=True), # (bs, 8, 8, 1024)
+    upsample(512, 4), # (bs, 16, 16, 1024)
+    upsample(256, 4), # (bs, 32, 32, 512)
+    upsample(128, 4), # (bs, 64, 64, 256)
+    upsample(64, 4), # (bs, 128, 128, 128)
+  ]
+
+  initializer = tf.random_normal_initializer(0., 0.02)
+  last = tf.keras.layers.Conv2DTranspose(OUTPUT_CHANNELS, 4,
+                                         strides=2,
+                                         padding='same',
+                                         kernel_initializer=initializer,
+                                         activation='tanh') # (bs, 256, 256, 3)
+
+  x = inputs
+
+  # Downsampling through the model
+  skips = []
+  for down in down_stack:
+    x = down(x)
+    skips.append(x)
+
+  skips = reversed(skips[:-1])
+
+  # Upsampling and establishing the skip connections
+  for up, skip in zip(up_stack, skips):
+    x = up(x)
+    x = tf.keras.layers.Concatenate()([x, skip])
+
+  x = last(x)
+
+  return tf.keras.Model(inputs=inputs, outputs=x)
+
+print('Loading GAN model')
+generator = Generator()
+generator.load_weights('gan_weight/generator')
+
+tf.keras.backend.set_floatx('float32')
+# # load GAN model
+filepath = "citygan.h5"
+
+generator = load_model(filepath)
+print('GAN model loaded')
+
+app = Flask(__name__)
+
+@app.route('/generate', methods=['POST'])
+def prediction_payload():
+    content = loads(request.json)['params']
+    
+    content = (np.array(content).reshape(256,256)-2)*(-1/2)
+    print(content.shape)
+    content = np.dstack((content, content, content))
+    input_img = np.expand_dims(content, axis = 0)
+    print(input_img[0,128,128,1])
+    # input_img = np.ones((1,256,256,3)).astype(float)
+
+    prediction = generator(input_img, training = True)[0]
+    filtered = np.array((prediction)).mean(axis = 2)
+    mask = np.where(filtered<0.5,0,1)
+
+    payload = dumps({'predicted':mask.tolist()})
+    return jsonify(payload)
+
+
+if __name__ == "__main__":
+    app.run(port=4453,debug=True)
